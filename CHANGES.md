b43031e9f451574fd6ba85d3843c6294afb258f1
glughi <giovanni.lughi@gmail.com>
2022-09-19 17:06:42 +0200

    Run prettier

--- CHANGES.md
+++ CHANGES.md
@@ -1,17 +1,20 @@
 # Change Log
 
 #### 2.0.0
-* Pull from to TerriaMap v8
-* Review and merge customizations from rer3d v7
-  * Add Rer and Nominatim search providers
-  * Italian language translation
-  * Add custom RER images and ico
-  * UI customization and translation
-  * Add RER tracking code
-  * Add domains to allowed proxy list
-  * Print version to console
-  * Remove RelatedMaps
+
+- Pull from to TerriaMap v8
+- Review and merge customizations from rer3d v7
+  - Add Rer and Nominatim search providers
+  - Italian language translation
+  - Add custom RER images and ico
+  - UI customization and translation
+  - Add RER tracking code
+  - Add domains to allowed proxy list
+  - Print version to console
+  - Remove RelatedMaps
+
 ### 2022-03-14
+
 ### 2022-06-30
 
 - Add support Plugins support using `terriajs-plugin-api`. See [`plugins.ts`](./plugins.ts)
--- index.js
+++ index.js
@@ -21,8 +21,8 @@ import BingMapsSearchProviderViewModel from "terriajs/lib/Models/SearchProviders
 // import GazetteerSearchProviderViewModel from 'terriajs/lib/ViewModels/GazetteerSearchProviderViewModel.js';
 // import GnafSearchProviderViewModel from 'terriajs/lib/ViewModels/GnafSearchProviderViewModel.js';
 // import defined from 'terriajs-cesium/Source/Core/defined';
-import RerSearchProviderViewModel from 'terriajs/lib/Models/SearchProviders/RerSearchProvider';
-import NominatimSearchProviderViewModel from 'terriajs/lib/Models/SearchProviders/NominatimSearchProvider';
+import RerSearchProviderViewModel from "terriajs/lib/Models/SearchProviders/RerSearchProvider";
+import NominatimSearchProviderViewModel from "terriajs/lib/Models/SearchProviders/NominatimSearchProvider";
 import render from "./lib/Views/render";
 import registerCatalogMembers from "terriajs/lib/Models/Catalog/registerCatalogMembers";
 import defined from "terriajs-cesium/Source/Core/defined";
@@ -89,19 +89,19 @@ module.exports = terria
     terria.loadInitSources().then((result) => result.raiseError(terria));
 
     try {
-        viewState.searchState.locationSearchProviders = [
-            new RerSearchProviderViewModel({ terria: terria }),
-            new NominatimSearchProviderViewModel({
-                terria: terria,
-                countryCodes: "it"
-            }),
-            new BingMapsSearchProviderViewModel({
-                terria: terria,
-                key: terria.configParameters.bingMapsKey
-            })
-            // new GazetteerSearchProviderViewModel({terria}),
-            // new GnafSearchProviderViewModel({terria})
-        ];
+      viewState.searchState.locationSearchProviders = [
+        new RerSearchProviderViewModel({ terria: terria }),
+        new NominatimSearchProviderViewModel({
+          terria: terria,
+          countryCodes: "it"
+        }),
+        new BingMapsSearchProviderViewModel({
+          terria: terria,
+          key: terria.configParameters.bingMapsKey
+        })
+        // new GazetteerSearchProviderViewModel({terria}),
+        // new GnafSearchProviderViewModel({terria})
+      ];
 
       // Automatically update Terria (load new catalogs, etc.) when the hash part of the URL changes.
       updateApplicationOnHashChange(terria, window);
