fcb103457b6a9a9231e1bc73abbbcfe9c6af6dfb
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2025-08-28 10:20:16 +0200

    Fix a bug due to es-toolkit

--- buildprocess/webpack.config.js
+++ buildprocess/webpack.config.js
@@ -34,7 +34,9 @@ module.exports = function (devMode, hot) {
             path.resolve(__dirname, "..", "index.js"),
             path.resolve(__dirname, "..", "entry.js"),
             path.resolve(__dirname, "..", "plugins.ts"),
-            path.resolve(__dirname, "..", "lib")
+            path.resolve(__dirname, "..", "lib"),
+            path.resolve(__dirname, "..", "node_modules", "es-toolkit"),
+            path.resolve(__dirname, "..", "node_modules", "recharts")
           ],
           use: [
             {
@@ -63,7 +65,9 @@ module.exports = function (devMode, hot) {
                   "@babel/proposal-class-properties",
                   "@babel/proposal-object-rest-spread",
                   "babel-plugin-styled-components",
-                  require.resolve("@babel/plugin-syntax-dynamic-import")
+                  require.resolve("@babel/plugin-syntax-dynamic-import"),
+                  "@babel/plugin-proposal-nullish-coalescing-operator",
+                  "@babel/plugin-proposal-optional-chaining"
                 ]
               }
             }
